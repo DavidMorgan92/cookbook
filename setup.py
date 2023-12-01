6c16d33da3fa40d491809dfd30ae42074fad22de
David Morgan <david.r.morgan.92@gmail.com>
2023-12-01 17:59:55 +0000

    Don't use MONGO_DBNAME setting

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ mongo = PyMongo()
 
 def create_app():
     app = Flask("Cook Book")
-    app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
     app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
     app.secret_key = os.environ.get("SECRET_KEY")
 
